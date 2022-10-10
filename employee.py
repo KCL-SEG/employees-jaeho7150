e292a82cfd7910fff626467e8ae695531c6907d8
Jaeho Lee <jaeho.lee@kcl.ac.uk>
2022-10-10 18:22:21 +0100

    add print 6 employees

--- employee.py
+++ employee.py
@@ -37,11 +37,13 @@ class Employee:
     def __str__(self):
         if self.contract_type == "hourly":
             if self.commision_contracts > 0:
-                return f"{self.name} works on a contract of {self.hours} at {self.hourly_pay}/hour and receives a commision for {self.commision_contracts} contract(s) at {self.commision_hourly_pay}/contract. Their total pay is {self.get_pay()}.
+                return f"{self.name} works on a contract of {self.hours} at {self.hourly_pay}/hour and receives a commision for {self.commision_contracts} contract(s) at {self.commision_hourly_pay}/contract. Their total pay is {self.get_pay()}."
             elif self.commision_bonus > 0:
                 return f"{self.name} works on a contract of {self.hours} at {self.hourly_pay}/hour and receives a bonus commision of {self.commision_bonus}. Their total pay is {self.get_pay()}."
             else:
-                return f"{self.name} works on a contract of {self.hours} at {self.hourly_pay}/hour. Their total pay is {self.get_pay()}"returneif self.contract_type == "monthly":
+                return f"{self.name} works on a contract of {self.hours} at {self.hourly_pay}/hour. Their total pay is {self.get_pay()}."
+
+        if self.contract_type == "monthly":
             if self.commision_contracts > 0:
                 return f"{self.name} works on a monthly salary of {self.monthly_salary} and receives a commision for {self.commision_contracts} contract(s) at {self.commision_hourly_pay}/contract. Their total pay is {self.get_pay()}."
             elif self.commision_bonus > 0:
@@ -50,8 +52,14 @@ class Employee:
                 return f"{self.name} works on a monthly salary of {self.monthly_salary}. Their total pay is {self.get_pay()}."
 
 billie = Employee("Billie", contract_type = "monthly", monthly_salary = 4000)
+print(str(billie))
 charlie = Employee("Charlie", contract_type = "hourly", hours = 100, hourly_pay = 25)
+print(str(charlie))
 renee = Employee("Renee", contract_type = "monthly", monthly_salary = 3000, commision_contracts = 4, commision_hourly_pay = 200)
+print(str(renee))
 jan = Employee("Jan", contract_type = "hourly", hours = 150, hourly_pay = 25, commision_contracts = 3, commision_hourly_pay = 220)
+print(str(jan))
 robbie = Employee("Robbie", contract_type = "monthly", monthly_salary = 2000, commision_bonus = 1500)
+print(str(robbie))
 ariel = Employee("Ariel", contract_type = "hourly", hours = 120, hourly_pay = 30, commision_bonus = 600)
+print(str(ariel))
